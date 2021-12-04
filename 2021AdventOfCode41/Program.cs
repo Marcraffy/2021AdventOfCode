75ec72594d746042d4ddb0958d58705148f068df
Marc Raftesath <marcraffy@gmail.com>
2021-12-04 12:45:16 +0200

    Add start of day 4 part 1 and add bingo to data

---
+++ 2021AdventOfCode41/Program.cs
@@ -0,0 +1,20 @@
+﻿using Data;
+using System;
+using System.Linq;
+
+namespace _2021AdventOfCode41
+{
+    class Program
+    {
+        private const bool DEBUG = true;
+
+        static void Main(string[] args)
+        {
+            var raw = Data.Data.GetData<BingoBoard>(DataFileKeys.BINGO);
+            var numbers = raw.First().Numbers;
+            var boards = raw.Select(board => board.Board).ToList();
+
+            //Console.WriteLine($"Life Support Rating: {lifeSupportRating}");
+        }
+    }
+}
--- Data/Data.cs
+++ Data/Data.cs
@@ -10,6 +10,7 @@ namespace Data
         public const string DEPTH_MEASUREMENTS = "DEPTH_MEASUREMENTS";
         public const string PATH = "PATH";
         public const string DIAGNOSTICS = "DIAGNOSTICS";
+        public const string BINGO = "BINGO";
     }
 
     public static class Data
@@ -19,13 +20,28 @@ namespace Data
             {DataFileKeys.DEPTH_MEASUREMENTS, "data.txt"},
             {DataFileKeys.PATH, "path.txt"},
             {DataFileKeys.DIAGNOSTICS, "binary.txt"},
+            {DataFileKeys.BINGO, "bingo.txt"},
         };
 
         public static List<T> GetData<T>(string dataFileKey) where T: class
         {
             var data = File.ReadAllText(DataFiles[dataFileKey]);
-            var output = data.Split('\n').Select(input => MapData<T>(dataFileKey, input.Trim())).ToList();
-            return output;
+            var dataSplitOnNewLine = data.Split('\n').Select(item => item.Trim());
+            switch (dataFileKey)
+            {
+                case DataFileKeys.BINGO:
+                    var numbers = dataSplitOnNewLine.First();
+                    var boards = dataSplitOnNewLine.Skip(1).Where(item => !string.IsNullOrWhiteSpace(item)).ToList();
+                    var bingoBoards = new List<T>();
+                    for (int index = 0; index < boards.Count/5; index+=5)
+                    {
+                        var board = boards.Skip(index).Take(5).Aggregate("", (acc, val) => acc + val + ' ');
+                        bingoBoards.Add(new BingoBoard(board, numbers) as T);
+                    }
+                    return bingoBoards;
+                default:
+                    return data.Split('\n').Select(input => MapData<T>(dataFileKey, input.Trim())).ToList();
+            }
         }
 
         private static T MapData<T>(string dataFileKey, string input) where T: class
@@ -37,6 +53,33 @@ namespace Data
                 DataFileKeys.DIAGNOSTICS => new Diagnostic(input) as T,
                 _ => null,
             };
+        }
+    }
+
+    public class BingoBoard
+    {
+
+        public BingoBoard(string input, string numbers)
+        {
+            Board = GetBoard(input);
+            Numbers = numbers.Split(',').Select(number => Convert.ToInt32(number)).ToArray();
+        }
+
+        public int[,] Board { get; set; }
+        public int[] Numbers { get; set; }
+
+        private int[,] GetBoard(string input)
+        {
+            var numbers = input.Split(' ').Where(number => !string.IsNullOrWhiteSpace(number)).Select(number => Convert.ToInt32(number)).ToList();
+            var board = new int[5, 5];
+            var row = 0;
+            for (int index = 0; index < 25; index++)
+            {
+                board[row, index % 5] = numbers[index];
+                if (index % 5 == 4)
+                    row++;
+            }
+            return board;
         }
     }
 
